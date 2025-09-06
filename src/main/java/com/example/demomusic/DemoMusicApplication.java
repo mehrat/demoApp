0f47705f37e2a96b76abcdd40816cdc84b9d8107
tanmaymehra <mehra.tanmay@gmail.com>
2025-09-06 18:11:49 +0530

    hello world api

---
+++ src/main/java/com/example/demomusic/DemoMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMusicApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demomusic/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demomusic.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Operation(summary = "Returns a Hello World message")
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
